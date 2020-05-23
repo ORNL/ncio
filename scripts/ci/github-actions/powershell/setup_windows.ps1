476a4d280a87bb7653cbbbb2645c85a74570067f
William F Godoy <williamfgc@yahoo.com>
2020-05-23 19:11:22 -0400

    Update meson location on Windows

--- scripts/ci/github-actions/powershell/setup_windows.ps1
+++ scripts/ci/github-actions/powershell/setup_windows.ps1
@@ -3,4 +3,4 @@
 choco install ninja -y
 py -3 -m pip install meson
 # Add pip scripts to PATH
-C:\hostedtoolcache\windows\Python\3.8.2\x64\Scripts\meson.exe --version
+C:\hostedtoolcache\windows\Python\3.8.3\x64\Scripts\meson.exe --version
