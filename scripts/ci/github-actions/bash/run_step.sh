56cc3dc8aec32ab51820af4fc39cc3f7433414bc
William F Godoy <godoywf@ornl.gov>
2021-03-06 10:53:21 -0500

    Update run_step.sh
    Back to gcovr

--- scripts/ci/github-actions/bash/run_step.sh
+++ scripts/ci/github-actions/bash/run_step.sh
@@ -39,8 +39,10 @@ case "$1" in
         CC=clang CXX=clang++ meson -Db_lundef=false -Db_sanitize=memory --prefix=${GITHUB_WORKSPACE}/../ncio-install ${GITHUB_WORKSPACE}
       ;;
       *"coverage"*)
-        echo 'Configure for code coverage with llvm-cov'
-        CC=clang CXX=clang++ meson -Dbuildtype=release -Db_coverage=true -Dc_args=-fno-elide-constructors --prefix=${GITHUB_WORKSPACE}/../ncio-install ${GITHUB_WORKSPACE}
+        echo 'Configure for code coverage with gcovr'
+        # regression bug in meson in 0.55
+        pip3 install meson==0.54.2
+        meson -Dbuildtype=release -Db_coverage=true -Dc_args=-fno-elide-constructors --prefix=${GITHUB_WORKSPACE}/../ncio-install ${GITHUB_WORKSPACE}
       ;;
       # Test with clang compilers
       *"clang"*)
