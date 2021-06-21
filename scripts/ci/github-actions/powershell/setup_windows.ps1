d747fe1cdeb4fdd609a4a8d591af19d148865066
William F Godoy <williamfgc@yahoo.com>
2021-06-21 11:44:04 -0400

    Update Windows VM installation location

--- scripts/ci/github-actions/powershell/setup_windows.ps1
+++ scripts/ci/github-actions/powershell/setup_windows.ps1
@@ -3,7 +3,7 @@
 choco install ninja -y
 py -3 -m pip install meson
 # Verify meson
-C:\hostedtoolcache\windows\Python\3.9.2\x64\Scripts\meson.exe --version
+C:\hostedtoolcache\windows\Python\3.9.5\x64\Scripts\meson.exe --version
 
 cmake --version
 # vcpkg install hdf5
